2c7a5a99f9b1e2028d00746e012956bc847861e9
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2023-05-31 09:27:56 +0200

    Include README.md as long_description

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
+from pathlib import Path
+
 from setuptools import find_packages, setup
+
+
+long_description = (Path(__file__).parent / 'README.md').read_text()
 
 
 dependencies = (
@@ -17,6 +22,9 @@ setup(
     name='lrbenchmark',
     version='0.1',
     author='Netherlands Forensics Institute',
+    description='Benchmarking Likelihood Ratio systems',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=dependencies,
 )
