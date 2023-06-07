c138a0e46e4b75c7a3db7d19b74bf8324c35562b
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2023-06-07 10:58:03 +0200

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ dependencies = (
 
 setup(
     name='lrbenchmark',
-    version='0.1',
+    version='0.1.1',
     author='Netherlands Forensics Institute',
     description='Benchmarking Likelihood Ratio systems',
     long_description=long_description,
