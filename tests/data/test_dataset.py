e929fd114fc7d5849fd9047d26b511eba2da7bee
roypm <r.ypma@nfi.nl>
2024-01-15 14:52:01 +0100

    added basic tests for dataset classes

--- tests/data/test_dataset.py
+++ tests/data/test_dataset.py
@@ -3,8 +3,10 @@ from typing import List
 import numpy as np
 import pytest
 
+from lrbenchmark.data.generated import SynthesizedNormalDataset
 from lrbenchmark.data.models import Measurement, Source
-from lrbenchmark.data.dataset import CommonSourceKFoldDataset
+from lrbenchmark.data.dataset import CommonSourceKFoldDataset, ASRDataset, GlassDataset
+import confidence
 
 @pytest.fixture
 def measurements() -> List[Measurement]:
@@ -21,6 +23,24 @@ def test_get_splits_is_mutually_exclusive(measurements):
         X_test, y_test = dataset_test.get_x_y_measurement()
         assert len(np.intersect1d(X_train, X_test)) == 0 and len(X_train) + len(X_test) == len(dataset.measurements)
         assert len(np.intersect1d(y_train, y_test)) == 0 and len(y_train) + len(y_test) == len(dataset.measurements)
+
+
+@pytest.mark.parametrize("class_name, config_key, load", [(ASRDataset, 'asr', True),
+                                               (GlassDataset, 'glass', True),
+                                               (SynthesizedNormalDataset, 'normal', False)])
+def test_dataset_basic_functions(class_name, config_key, load):
+    config = confidence.load_name('tests/lrbenchmark_test')
+
+    dataset = class_name(**config.dataset_test[config_key])
+    if load:
+        dataset.load()
+    else:
+        dataset = dataset.generate_data(1000)
+
+    dataset.get_splits()
+
+
+
 
 
 
