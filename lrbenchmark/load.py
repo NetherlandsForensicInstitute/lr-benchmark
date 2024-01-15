b336138b5d4910ca4d58f7b2fed30d410bbd5867
Claire Wagenaar <135599108+cdwagenaar@users.noreply.github.com>
2024-01-15 15:26:10 +0100

    config structure to load data

---
+++ lrbenchmark/load.py
@@ -0,0 +1,27 @@
+import argparse
+
+from confidence import Configuration, loadf
+
+
+def get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-d',
+        '--data-config',
+        type=str,
+        required=True,
+        help="The path to a .yaml file containing the configuration for the dataset to be used."
+    )
+    return parser
+
+
+def load_data_config(config_name: str) -> Configuration:
+    """Load the config file for the provided `config_name`. This can either be
+    the name of the yaml or the full path."""
+    if not config_name.endswith(".yaml"):
+        config_name += ".yaml"
+    if not config_name.startswith('config'):
+        if not config_name.startswith('data'):
+            config_name = 'data/' + config_name
+        config_name = 'config/' + config_name
+    return loadf(config_name)
