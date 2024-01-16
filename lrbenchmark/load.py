e6c230a9109b1ee02b49bd2a668ddee42a0d00a2
Claire Wagenaar <135599108+cdwagenaar@users.noreply.github.com>
2024-01-16 10:18:38 +0100

    also possible to load existing file

--- lrbenchmark/load.py
+++ lrbenchmark/load.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 from confidence import Configuration, loadf
 
@@ -15,15 +16,24 @@ def get_parser() -> argparse.ArgumentParser:
     return parser
 
 
-def load_data_config(config_name: str) -> Configuration:
+def load_data_config(path: str) -> Configuration:
     """
-    Load the data config file for the provided `config_name`. This can either be
+    Load the data config file for the provided `path`. This can either be
     the name of the yaml itself or the full path.
     """
-    if not config_name.endswith('.yaml'):
-        config_name += '.yaml'
-    if not config_name.startswith('config'):
-        if not config_name.startswith('data'):
-            config_name = 'data/' + config_name
-        config_name = 'config/' + config_name
-    return loadf(config_name)
+    if not os.path.isfile(path):
+        path = get_data_config_path(path)
+    return loadf(path)
+
+
+def get_data_config_path(path: str) -> str:
+    """
+    Extract the actual path to a yaml file located in the config folder.
+    """
+    if not path.endswith('.yaml'):
+        path += '.yaml'
+    if not path.startswith('config'):
+        if not path.startswith('data'):
+            path = 'data/' + path
+        path = 'config/' + path
+    return path
