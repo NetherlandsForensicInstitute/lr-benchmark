6076366d1e9bf6d5ae8d8d9bf455d7046a8fa75c
Simone <simone_ariens@hotmail.com>
2024-01-29 13:13:04 +0100

    preprocessor configurable and data config removed from fit_predict (#30)
    * preprocessor configurable and data config removed from fit_predict
    
    ---------
    
    Co-authored-by: Simone Ariens <s.ariens@nfi.nl>

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from confidence import Configuration
 from lir import calculate_lr_statistics, Xy_to_Xn
-from sklearn.base import TransformerMixin, BaseEstimator
+from sklearn.base import BaseEstimator
 from sklearn.metrics import roc_auc_score
 from tqdm import tqdm
 
@@ -21,11 +21,10 @@ from lrbenchmark.pairing import BasePairing, CartesianPairing
 from lrbenchmark.refnorm import perform_refnorm
 from lrbenchmark.transformers import BaseScorer
 from lrbenchmark.utils import get_experiment_description, prepare_output_file
-from params import SCORERS, CALIBRATORS, DATASETS, PREPROCESSORS, get_parameters, PAIRING
+from params import parse_config, config_option_dicts
 
 
 def fit_and_evaluate(dataset: Dataset,
-                     preprocessor: TransformerMixin,
                      pairing_function: BasePairing,
                      calibrator: BaseEstimator,
                      scorer: BaseScorer,
@@ -45,16 +44,14 @@ def fit_and_evaluate(dataset: Dataset,
     for idx in tqdm(range(repeats), desc=', '.join(map(str, selected_params.values())) if selected_params else ''):
         # split off the sources that should only be evaluated
         holdout_set, dataset = dataset.split_off_holdout_set()
-        if refnorm.refnorm_size:
-            dataset, dataset_refnorm = next(dataset.get_splits(validate_size=refnorm.refnorm_size, seed=idx))
+        if refnorm and refnorm['refnorm_size']:
+            dataset, dataset_refnorm = next(dataset.get_splits(validate_size=refnorm['refnorm_size'], seed=idx))
         for dataset_train, dataset_validate in dataset.get_splits(seed=idx,
-                                                                  **experiment_config.experiment.splitting_strategy):
+                                                                  **experiment_config['splitting_strategy']):
             train_pairs = dataset_train.get_pairs(pairing_function=pairing_function, seed=idx)
             validate_pairs = dataset_validate.get_pairs(pairing_function=pairing_function, seed=idx)
 
-            # todo: what to do with the preprocessor?
-            # todo: another way to get the paths in the scorer?
-            train_scores = scorer.fit_predict(train_pairs, experiment_config.dataset)
+            train_scores = scorer.fit_predict(train_pairs)
             validation_scores = scorer.predict(validate_pairs)
 
             if refnorm:
@@ -99,21 +96,22 @@ def fit_and_evaluate(dataset: Dataset,
     return results
 
 
-def run(exp: evaluation.Setup, exp_config: Configuration) -> None:
+def run(exp: evaluation.Setup, config: Configuration) -> None:
     """
     Executes experiments and saves results to file.
     :param exp: Helper class for execution of experiments.
-    :param exp_config: Experiment parameters.
+    :param config: configuration
     """
-    exp_params = exp_config.experiment
-    exp.parameter('repeats', exp_params.repeats)
-    exp.parameter('refnorm', exp_params.refnorm)
+
+    config_resolved = parse_config(config, config_option_dicts)
+    exp_config = config_resolved['experiment']
+    exp.parameter('repeats', exp_config['repeats'])
+    exp.parameter('refnorm', exp_config.get('refnorm', None))
     exp.parameter('experiment_config', exp_config)
-    parameters = {'dataset': get_parameters(exp_config.dataset, DATASETS),
-                  'pairing_function': get_parameters(exp_params.pairing, PAIRING),
-                  'preprocessor': get_parameters(exp_params.preprocessor, PREPROCESSORS),
-                  'scorer': get_parameters(exp_params.scorer, SCORERS),
-                  'calibrator': get_parameters(exp_params.calibrator, CALIBRATORS)}
+    exp.parameter('dataset', config_resolved['dataset'])
+    parameters = {'pairing_function': exp_config['pairing'],
+                  'scorer': exp_config['scorer'],
+                  'calibrator': exp_config['calibrator']}
 
     if [] in parameters.values():
         raise ValueError('Every parameter should have at least one value, '
@@ -147,7 +145,7 @@ def run(exp: evaluation.Setup, exp_config: Configuration) -> None:
     # save figures and results per parameter set
     for result_row, param_set in zip(agg_result, param_sets):
         for fig_name, fig in result_row['figures'].items():
-            short_description = ' - '.join([str(val)[:5] for val in param_set.values()])
+            short_description = ' - '.join([val.__class__.__name__[:5] for val in param_set.values()])
             path = f'{folder_name}/{short_description}/{fig_name}'
             prepare_output_file(path)
             fig.savefig(path)
