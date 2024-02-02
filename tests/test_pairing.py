dac99a774a309585b91731adeb17f8127c941287
Claire Wagenaar <135599108+cdwagenaar@users.noreply.github.com>
2024-02-02 10:14:37 +0100

    pass test

--- tests/test_pairing.py
+++ tests/test_pairing.py
@@ -5,7 +5,8 @@ from lrbenchmark.pairing import CartesianPairing
 def test_split_by_reference(measurements, measurements_set2):
     dataset = Dataset(measurements=measurements + measurements_set2)
     pairing_function = CartesianPairing()
-    pairs = dataset.get_pairs(pairing_function=pairing_function, seed=0, distinguish_trace_reference=True)
+    pairs = dataset.get_pairs(pairing_function=pairing_function, seed=0,
+                              pair_should_have_trace_and_reference_measurements=True)
     for pair in pairs:
         assert (pair.measurement_a.is_like_reference and pair.measurement_b.is_like_trace) or (
                 pair.measurement_a.is_like_trace and pair.measurement_b.is_like_reference)
