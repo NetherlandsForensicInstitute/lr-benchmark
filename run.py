77394ec764fec5a94e68a5b284f5f3cf8c76c4a9
Claire Wagenaar <135599108+cdwagenaar@users.noreply.github.com>
2024-02-06 14:35:16 +0100

    write yaml to output folder

--- run.py
+++ run.py
@@ -182,6 +182,8 @@ def run(exp: evaluation.Setup, config: Configuration) -> None:
             prepare_output_file(path)
             fig.savefig(path)
 
+    # save yaml configuration file
+    confidence.dumpf(config, f'{folder_name}/config.yaml')
 
 if __name__ == '__main__':
     parser = get_parser()
