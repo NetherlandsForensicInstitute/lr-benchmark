3dfc78fb8671d2550cc5dc93c79caa4b9862ff6a
Simone <simone_ariens@hotmail.com>
2024-03-01 09:46:28 +0100

    fixed folders and added run ids (#50)
    * fixed folders and added run ids
    
    * cleanup write metrics
    
    * fixed refnorm stats
    
    ---------
    
    Co-authored-by: Simone Ariens <s.ariens@nfi.nl>

--- lrbenchmark/io.py
+++ lrbenchmark/io.py
@@ -1,6 +1,6 @@
 import csv
 from pathlib import Path
-from typing import List, Dict
+from typing import List, Mapping
 
 from lrbenchmark.typing import Result
 
@@ -10,46 +10,46 @@ def prepare_output_file(path: str) -> str:
     return path
 
 
-def write_metrics(agg_result: List[Result], folder_name: str):
+def write_metrics(agg_result: List[Result], param_sets: List[Mapping], folder_name: str):
     with open(prepare_output_file(f'{folder_name}/all_metrics.csv'), 'w') as file:
-        fieldnames = sorted(set(agg_result[0].metrics.keys()))
+        fieldnames = ['run'] + sorted(param_sets[0].keys()) + sorted(agg_result[0].metrics.keys())
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader()
-        for result_row in agg_result:
+        for i, (result_row, param_set) in enumerate(zip(agg_result, param_sets)):
+            params_and_results = dict(param_set, **result_row.metrics)
+            params_and_results['run'] = i
             writer.writerow(
-                {fieldname: value for fieldname, value in result_row.metrics.items() if fieldname in fieldnames})
+                {fieldname: value for fieldname, value in params_and_results.items() if fieldname in fieldnames})
 
 
 def write_calibration_results(agg_result: List[Result], folder_name: str):
     with open(prepare_output_file(f'{folder_name}/calibration_results.csv'), 'w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(['pair', 'normalized_score', 'is_same_source'])
-        for result_row in agg_result:
-            writer.writerows(result_row.calibration_results)
+        writer.writerow(['run', 'pair', 'normalized_score', 'is_same_source'])
+        for i, result_row in enumerate(agg_result):
+            writer.writerows([[i] + r for r in result_row.calibration_results])
 
 
 def write_lrs(agg_result: List[Result], folder_name: str):
     with open(prepare_output_file(f'{folder_name}/holdout_lrs.csv'), 'w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(['desc', 'pair', 'LR'])
-        for result_row in agg_result:
+        writer.writerow(['run', 'pair', 'LR'])
+        for i, result_row in enumerate(agg_result):
             for pair_desc, lr in result_row.holdout_lrs.items():
-                writer.writerow([result_row.metrics['desc'], pair_desc, lr])
+                writer.writerow([i, pair_desc, lr])
 
 
 def write_refnorm_stats(agg_result: List[Result], folder_name: str):
     with open(prepare_output_file(f'{folder_name}/refnorm_source_ids.csv'), 'w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(['run', 'source_ids'])
-        for result_row in agg_result:
-            for run, sources in result_row.refnorm_stats.items():
-                writer.writerow([f'run_{run}', *sources])
+        writer.writerow(['run', 'repeat', 'source_ids'])
+        for i, result_row in enumerate(agg_result):
+            for repeat, sources in result_row.refnorm_stats.items():
+                writer.writerow([i, repeat, *sources])
 
 
-def save_figures_per_param_set(agg_result: List[Result], param_sets: List[Dict], folder_name: str):
-    for result_row, param_set in zip(agg_result, param_sets):
-        for fig_name, fig in result_row.figures.items():
-            short_description = ' - '.join([val.__class__.__name__[:5] for val in param_set.values()])
-            path = f'{folder_name}/{short_description}/{fig_name}'
-            prepare_output_file(path)
-            fig.savefig(path)
+def save_figures(result: Result, folder_name: str):
+    for fig_name, fig in result.figures.items():
+        path = f'{folder_name}/{fig_name}'
+        prepare_output_file(path)
+        fig.savefig(path)
--- lrbenchmark/utils.py
+++ lrbenchmark/utils.py
@@ -1,27 +1,5 @@
-from typing import Dict, Any, Optional, Mapping, Tuple
+from typing import Any, Optional, Mapping, Tuple
 from lrbenchmark.data.models import MeasurementPair
-
-
-def get_experiment_description(selected_params: Optional[Dict[str, Any]]) -> str:
-    """
-    Concatenates information about the parameters of the experiment into a string.
-    :param selected_params: dict of selected parameters and their values
-    :return: experiment description (str)
-    """
-    if selected_params:
-        string_agg = []
-        for param_name, param_value in selected_params.items():
-            if isinstance(param_value, int):
-                string_agg.append(f'{param_name}={param_value}')
-            elif hasattr(param_value, 'calibrator'):
-                string_agg.append(f'{param_name}={str(param_value.__class__)}_{param_value.calibrator}')
-            elif hasattr(param_value, 'first_step_calibrator'):
-                string_agg.append(f'{param_name}={str(param_value.__class__)}_{param_value.first_step_calibrator}')
-            else:
-                string_agg.append(f'{param_name}={param_value}')
-        return '_'.join(string_agg)
-    else:
-        return "defaults"
 
 
 def pair_complies_with_properties(measurement_pair: MeasurementPair,
