b6cef1e6d2064a9a8f61ad4ca67abe74952bc465
Claire Wagenaar <135599108+cdwagenaar@users.noreply.github.com>
2024-03-28 14:13:59 +0100

    review comments

--- lrbenchmark/io.py
+++ lrbenchmark/io.py
@@ -35,7 +35,7 @@ def write_calibration_results(agg_result: List[Result], folder_name: str):
             writer.writerows([[i] + r for r in result_row.calibration_results])
 
 
-def save_lr_results(agg_result: List[Result], folder_name: str):
+def save_holdout_results(agg_result: List[Result], folder_name: str):
     with open(prepare_output_file(f'{folder_name}/holdout_lrs.csv'), 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(['run', 'pair', 'LR'])
@@ -44,20 +44,18 @@ def save_lr_results(agg_result: List[Result], folder_name: str):
             for pair_desc, lr in result_row.holdout_lrs.items():
                 writer.writerow([i, pair_desc, lr])
                 path = f'{folder_name}/holdout_figs/{i}_{pair_desc}.png'
-                save_lr_distribution_fig(fig, np.log10(lr), path)
+                plot_lr_on_distribution(fig, np.log10(lr), path)
 
 
-def save_lr_distribution_fig(fig: plt.Figure, log_lr: float, path: str):
+def plot_lr_on_distribution(fig: plt.Figure, log_lr: float, path: str):
     """
-    Plot the LR distribution histograms with the actual logLR as vertical line and save.
+    Plot the LR distribution histograms with the actual logLR as vertical line and save the figure.
     """
-    ax2 = fig.axes[0].twinx()  # copy the x-axis of the histogram plot
-    ax2.axvline(log_lr, 0, 0.95)  # plot a vertical line at the log_lr
-    ax2.get_yaxis().set_visible(False)  # remove the new y-axis
-    ax2.set_title(f'{LOG_LR}: {log_lr}')
     prepare_output_file(path)
-    fig.savefig(path)
-    ax2.remove()  # remove the new axis so the original figure remains unchanged
+    vline = fig.axes[0].axvline(log_lr, 0, 0.95)
+    fig.axes[0].set_title(f'{LOG_LR}: {log_lr}')
+    fig.savefig(path, bbox_inches='tight')
+    vline.remove()
 
 
 def write_refnorm_stats(agg_result: List[Result], folder_name: str):
