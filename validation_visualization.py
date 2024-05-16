c18c1dac12127c236f18baaef07721e7a939518e
Joke Kalter <j.kalter@nfi.nl>
2024-05-16 13:48:47 +0200

    Review comments processed

--- validation_visualization.py
+++ validation_visualization.py
@@ -5,61 +5,78 @@ import pandas as pd
 import yaml
 
 import streamlit as st
+from pandas import DataFrame
 
 
 def get_pairing_properties(experiment_folder: Path, group: str):
     run_folder = list(experiment_folder.glob(f'{group}_*'))
     if len(run_folder) > 1:
-        print(f"Found more than 1 possible output folder for run group {group}:\n'\n'.join(run_folder)")
-        return None
+        folders = '\n'.join([str(f) for f in run_folder])
+        st.warning(f"Found more than 1 possible output folder for run group {group}:\n{folders}")
+        return "Warning: multiple pairing properties found"
     elif len(run_folder) == 0:
-        print(f"Found no possible output folder for run group {group} in directory {experiment_folder}")
-        return None
+        st.warning(f"Found no possible output folder for run group {group} in directory {experiment_folder}")
+        return "Warning: no pairing properties found"
     else:
         f = run_folder[0] / 'run_config.yaml'
         props = yaml.safe_load(f.read_text())
         return props['pairing_properties']
 
 
-experiment_folders = [p.name for p in Path('./output').glob('*')]
-
-experiment = st.selectbox('Select experiment', experiment_folders)
-
-experiment_folder = Path(f'./output/{experiment}')
-calibration_results_path = experiment_folder / 'calibration_results.csv'
-
-
 @st.cache_data
-def get_calibration_results(path):
+def get_calibration_results(path: Path):
     data = pd.read_csv(path)
-    data['run'] = data['run'].astype(str)
-    data['llrs'] = data['lrs'].apply(lambda x: np.log10(x))
-    distinct_groups = data['run'].unique()
-    groups_with_labels = {
-        group: get_pairing_properties(experiment_folder, group) for group in
-        distinct_groups}
-    return data, distinct_groups, groups_with_labels
+    if 'lr' in data.columns:
+        data['run'] = data['run'].astype(str)
+        data['llrs'] = data['lr'].apply(lambda x: np.log10(x))
+        distinct_groups = data['run'].unique()
+        groups_with_labels = {
+            group: get_pairing_properties(experiment_folder, group) for group in
+            distinct_groups}
+        return data, distinct_groups, groups_with_labels
+    else:
+        return None, None, None
 
-
-calibration_results, groups, labels = get_calibration_results(calibration_results_path)
 
 @st.cache_data
-def downsample(data):
-    data['round_score'] = data['normalized_score'].apply(lambda x: round(x))
+def downsample(data: DataFrame, n_decimals: int = 2):
+    data['round_score'] = data['normalized_score'].apply(lambda x: round(x, n_decimals))
     downsampled = calibration_results.groupby(['run', 'round_score']).first()
     return downsampled.reset_index()
 
 
-downsampled_results = downsample(calibration_results)
+experiment_folders = sorted([p.name for p in Path('./output').glob('*')], reverse=True)
 
-st.write('Select groups to compare:')
-for group in groups:
-    st.checkbox(f"{group}: {labels[group]}", key=group, value=True)
+experiment = st.selectbox('Select experiment', experiment_folders)
 
-selected_groups = [key for key in st.session_state.keys() if st.session_state[key]]
+experiment_folder = Path(f'./output/{experiment}')
+calibration_results_file = experiment_folder / 'calibration_results.csv'
 
-selected_data = downsampled_results[downsampled_results['run'].isin(selected_groups)]
+n_decimals = st.selectbox("Downsample specificity ('None' for no downsampling, might be slow to process)", [0, 1, 2, 3, None])
 
-st.header('Scores to log LR per ')
-st.scatter_chart(selected_data, x='normalized_score', y='llrs', color='run')
+if calibration_results_file.exists():
+    calibration_results, groups, labels = get_calibration_results(calibration_results_file)
 
+    if type(calibration_results) is DataFrame:
+        if n_decimals is not None:
+            downsampled_results = downsample(calibration_results, n_decimals)
+        else:
+            downsampled_results = calibration_results
+
+        st.write('Select groups to compare:')
+        for group in groups:
+            st.checkbox(f"{group}: {labels[group]}", key=group, value=True)
+
+        selected_groups = [key for key in st.session_state.keys() if st.session_state[key]]
+
+        selected_data = downsampled_results[downsampled_results['run'].isin(selected_groups)]
+
+        st.header('Scores to log10 LR per property pairing')
+        st.scatter_chart(selected_data, x='normalized_score', y='llrs', color='run')
+
+    else:
+        st.warning(f"File '{calibration_results_file}' does not contain the 'lr' column. "
+                   f"Run the latest version of run.py to include this column in the results.")
+
+else:
+    st.warning(f"File '{calibration_results_file}' not found")
